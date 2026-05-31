5a864dc18f81ddea69e663b6d25a80923e657afc
averageanime <nkarmartsangschool@gmail.com>
2026-05-31 16:33:46 -0700

    Phase 9: Split creative tabs into food items and fluids
    Separates fluid buckets into a dedicated 'Create: Food Fluids' tab,
    keeping food items and blocks in the main 'Create: Food' tab.
    Display blocks tab will be added in Phase 11.

--- src/main/java/net/averageanime/createfood/creativetab/ModCreativeTab.java
+++ src/main/java/net/averageanime/createfood/creativetab/ModCreativeTab.java
@@ -4,6 +4,7 @@ import com.simibubi.create.AllCreativeModeTabs;
 import com.tterrag.registrate.util.entry.RegistryEntry;
 import it.unimi.dsi.fastutil.objects.ReferenceArrayList;
 import it.unimi.dsi.fastutil.objects.ReferenceLinkedOpenHashSet;
+import net.averageanime.createfood.fluid.ModFluids;
 import net.averageanime.createfood.item.ModItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -14,7 +15,6 @@ import net.minecraftforge.fluids.ForgeFlowingFluid;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.averageanime.createfood.CreateFood;
-import vectorwing.farmersdelight.common.block.PieBlock;
 
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -26,6 +26,7 @@ public class ModCreativeTab {
 
     private static final DeferredRegister<CreativeModeTab> REGISTER;
     public static final RegistryObject<CreativeModeTab> CREATIVE_TAB;
+    public static final RegistryObject<CreativeModeTab> FLUID_TAB;
 
     public ModCreativeTab() {}
 
@@ -35,78 +36,74 @@ public class ModCreativeTab {
 
     static {
         REGISTER = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, CreateFood.ID);
-        CREATIVE_TAB = REGISTER.register("base", () -> {
-            return CreativeModeTab.builder().title(Component.literal("Create: Food"))
-                    .withTabsBefore(AllCreativeModeTabs.BASE_CREATIVE_TAB.getKey(),AllCreativeModeTabs.PALETTES_CREATIVE_TAB.getKey())
+        CREATIVE_TAB = REGISTER.register("base", () ->
+            CreativeModeTab.builder()
+                    .title(Component.literal("Create: Food"))
+                    .withTabsBefore(AllCreativeModeTabs.BASE_CREATIVE_TAB.getKey(), AllCreativeModeTabs.PALETTES_CREATIVE_TAB.getKey())
                     .icon(() -> new ItemStack(ModItems.BREAKFAST_PLATE.get()))
-                    .displayItems(new DisplayItemsGenerator())
-                    .build();
-        });
+                    .displayItems((params, output) -> {
+                        List<Item> items = new LinkedList<>();
+                        items.addAll(collectItems());
+                        items.addAll(collectBlocks());
+                        filterAndOutput(output, items);
+                    })
+                    .build()
+        );
+        FLUID_TAB = REGISTER.register("fluid", () ->
+            CreativeModeTab.builder()
+                    .title(Component.literal("Create: Food Fluids"))
+                    .withTabsBefore(CREATIVE_TAB.getKey())
+                    .icon(() -> new ItemStack(ModFluids.CUSTARD.getBucket().orElse(Items.AIR)))
+                    .displayItems((params, output) -> filterAndOutput(output, collectFluids()))
+                    .build()
+        );
     }
 
-    private static class DisplayItemsGenerator implements CreativeModeTab.DisplayItemsGenerator {
-
-        @Override
-        public void accept(CreativeModeTab.ItemDisplayParameters itemDisplayParameters, CreativeModeTab.Output output) {
-            List<Item> items = new LinkedList();
-            items.addAll(this.collectItems());
-            items.addAll(this.collectBlocks());
-            items.addAll(this.collectFluid());
-            filterAndOutput(output, items);
-        }
-
-        private List<Item> collectBlocks() {
-            List<Item> items = new ReferenceArrayList();
-            Iterator var3 = REGISTRATE.getAll(Registries.BLOCK).iterator();
-
-            while(var3.hasNext()) {
-                RegistryEntry<Block> entry = (RegistryEntry)var3.next();
-                Item item = entry.get().asItem();
-                if (item != Items.AIR) {
-                    items.add(item);
-                }
+    private static List<Item> collectBlocks() {
+        List<Item> items = new ReferenceArrayList<>();
+        for (RegistryEntry<Block> entry : REGISTRATE.getAll(Registries.BLOCK)) {
+            Item item = entry.get().asItem();
+            if (item != Items.AIR) {
+                items.add(item);
             }
-
-            items = new ReferenceArrayList(new ReferenceLinkedOpenHashSet(items));
-            return items;
         }
+        return new ReferenceArrayList<>(new ReferenceLinkedOpenHashSet<>(items));
+    }
 
-        private List<Item> collectItems() {
-            List<Item> items = new ReferenceArrayList();
-            for (RegistryObject<Item> entry : ModItems.ITEMS.getEntries()) {
-                Item item = entry.get();
-                if (!(item instanceof BlockItem) && !(item instanceof BucketItem)) {
-                    items.add(item);
-                }
+    private static List<Item> collectItems() {
+        List<Item> items = new ReferenceArrayList<>();
+        for (RegistryObject<Item> entry : ModItems.ITEMS.getEntries()) {
+            Item item = entry.get();
+            if (!(item instanceof BlockItem) && !(item instanceof BucketItem)) {
+                items.add(item);
             }
-            return items;
         }
+        return items;
+    }
 
-        private List<Item> collectFluid() {
-            List<Item> items = new ReferenceArrayList();
-            Iterator var3 = REGISTRATE.getAll(Registries.FLUID).iterator();
-
-            while(var3.hasNext()) {
-                RegistryEntry<ForgeFlowingFluid> entry = (RegistryEntry)var3.next();
+    @SuppressWarnings("unchecked")
+    private static List<Item> collectFluids() {
+        List<Item> items = new ReferenceArrayList<>();
+        Iterator var3 = REGISTRATE.getAll(Registries.FLUID).iterator();
+        while (var3.hasNext()) {
+            RegistryEntry<ForgeFlowingFluid> entry = (RegistryEntry<ForgeFlowingFluid>) var3.next();
+            try {
                 ForgeFlowingFluid fluid = entry.get();
-                if (fluid.getBucket()!=Items.AIR && !items.contains(fluid.getBucket())) {
+                if (fluid.getBucket() != Items.AIR && !items.contains(fluid.getBucket())) {
                     items.add(fluid.getBucket());
                 }
-            }
-
-            return items;
+            } catch (ClassCastException ignored) {}
         }
+        return items;
+    }
 
-        private static void filterAndOutput(CreativeModeTab.Output output, List<Item> items) {
-            Iterator var4 = items.iterator();
-            while(var4.hasNext()) {
-                Item item = (Item)var4.next();
-                if(item.toString().contains("incomplete")) continue;
-                if(item.toString().contains("guide")) continue;
-                if(item.toString().contains("blaze_burner")) continue;
-                if(item.toString().contains("creative_tab_icon")) continue;
-                output.accept(item);
-            }
+    private static void filterAndOutput(CreativeModeTab.Output output, List<Item> items) {
+        for (Item item : items) {
+            if (item.toString().contains("incomplete")) continue;
+            if (item.toString().contains("guide")) continue;
+            if (item.toString().contains("blaze_burner")) continue;
+            if (item.toString().contains("creative_tab_icon")) continue;
+            output.accept(item);
         }
     }
 }
